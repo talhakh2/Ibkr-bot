3a260e1fb8cf1615db3b30d29aa0ac3ed17a8bfd
talhakh2 <talha.kh58@gmail.com>
2025-02-25 00:40:52 +0500

    first commit

--- helper/wait.py
+++ helper/wait.py
@@ -1,19 +1,20 @@
 from helper.db_connection import trades_collection
-from datetime import datetime
+from datetime import datetime, timezone
 from bson import ObjectId
 import time
-import asyncio
 
 def wait_until(target_time: datetime, mongo_id) -> bool:
-    print("Waiting until:", target_time)
-    trade_record = trades_collection.find_one({"_id": ObjectId(mongo_id)})
-    print("datetime.now(): ", datetime.now())
-    print("target_time: ", target_time)
-    while datetime.now() < target_time:
-        print("current_time: ", datetime.now())
+    target_time = target_time.astimezone(timezone.utc)  # Convert to UTC
+    print("Waiting until (UTC):", target_time)
+
+    while datetime.now(timezone.utc) < target_time:
+        trade_record = trades_collection.find_one({"_id": ObjectId(mongo_id)})
+        print("Current UTC time:", datetime.now(timezone.utc))
+
         if trade_record and trade_record.get("cancel_requested"):
-            print("Current time: ", datetime.now())
-            print(f"Trade {mongo_id} was canceled.")
+            print(f"Trade {mongo_id} was canceled at {datetime.now(timezone.utc)}.")
             return False  # Stop waiting, trade was canceled
+
         time.sleep(1)
+
     return True
